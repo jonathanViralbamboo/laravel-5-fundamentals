7a0ff0673a8b35736e9437b1c58661a222a9a07d
jonathanViralbamboo <jonathan@viralbamboo.com>
2017-01-04 11:38:27 +0000

    Project Start, About Page
    Project starting files with basic about page passing data

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <title>Document</title>
+  </head>
+  <body>
+    <h1>About Me: {{ $first }} {{ $last }}</h1>
+    <p>About me paragraph!</p>
+  </body>
+</html>
