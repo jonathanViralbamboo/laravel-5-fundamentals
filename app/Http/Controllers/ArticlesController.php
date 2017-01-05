19e0809078cfa92ae280336fdeeb8193adf6cc06
jonathanViralbamboo <jonathan@viralbamboo.com>
2017-01-05 14:56:37 +0000

    Forms
    - Added laravelCollective/html.
    Created a form to add articles and display articles by latest

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Article;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 
-use Illuminate\Http\Request;
+use Request;
 
 class ArticlesController extends Controller
 {
     // Display all articles
     public function index()
     {
-        $articles = Article::all();
+        $articles = Article::latest('published_at')->get();
 
         return view('articles.index')->with('articles', $articles);
     }
@@ -23,5 +24,27 @@ class ArticlesController extends Controller
       $article = Article::findOrFail($id);
 
       return view('articles.show', compact('article'));
+    }
+
+    // Create a new article
+    public function create()
+    {
+      return view('articles.create');
+    }
+
+    public function store()
+    {
+      $input = Request::all();
+      $input['published_at'] = Carbon::now();
+
+      // $article = new Article;
+      // $article->title = $input['title'];
+      // $article->body = $input['body'];
+
+      // OR
+
+      Article::create($input);
+
+      return redirect('articles');
     }
 }
