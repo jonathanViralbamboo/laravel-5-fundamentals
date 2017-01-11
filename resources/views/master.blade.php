9eb74bee40366bf4d75b2e510678a4b696525d51
jonathanViralbamboo <jonathan@viralbamboo.com>
2017-01-11 17:32:55 +0000

    Select2
    Using the Select2 plugin to clean up the tags selection in the forms

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8"/>
     <title>Document</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
   </head>
   <body>
     <div class="container">
@@ -14,6 +15,7 @@
 
     <script src="//code.jquery.com/jquery.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
 
     <script>
       $('#flash-overlay-modal').modal();
